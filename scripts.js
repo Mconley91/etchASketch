ec670c1817705d0f3923b99ea115cd4b61e6dd2c
mconley91 <matthew.e.conley@gmail.com>
2024-10-31 15:21:46 -0400

    add for loop to create 256 squares for basic 'coloring' feature

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 const screen = document.querySelector('#screen');
+for(let i = 0; i < 256; i++){
+    const square = document.createElement('div');
+    square.setAttribute('id', `square${i}`);
+    square.style.backgroundColor = 'white';
+    square.style.width = '50px';
+    square.style.height = '50px';
+    square.addEventListener("mouseover",()=>{square.style.backgroundColor = 'black'})
+    screen.appendChild(square);
+}
