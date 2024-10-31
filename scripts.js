77796856f4ef7f35e52b497ef85fa106d184a13b
mconley91 <matthew.e.conley@gmail.com>
2024-10-31 19:25:23 -0400

    add color options, reset button, adjusted screen square size to be proportional

--- scripts.js
+++ scripts.js
@@ -1,10 +1,44 @@
 const screen = document.querySelector('#screen');
-for(let i = 0; i < 256; i++){
+const buttons = document.querySelectorAll('.colorButton')
+let color = 'black';
+
+for(let i = 0; i < 1000; i++){
     const square = document.createElement('div');
     square.setAttribute('id', `square${i}`);
+    square.setAttribute('class', `square`);
     square.style.backgroundColor = 'white';
-    square.style.width = '50px';
-    square.style.height = '50px';
-    square.addEventListener("mouseover",()=>{square.style.backgroundColor = 'black'})
+    square.style.width = '2%';
+    square.style.height = '5%';
+    square.addEventListener("mouseover",()=>{square.style.backgroundColor = color})
     screen.appendChild(square);
+};
+
+buttons.forEach((button)=>{
+    button.addEventListener('click',(event)=>{
+        switch(event.target.id){
+            case 'yellow':
+                color = 'yellow';
+                break;
+            case 'green':
+                color = 'green';
+                break;
+            case 'red':
+                color = 'red';
+                break;
+            case 'blue':
+                color = 'blue';
+                break;
+            case 'black':
+                color = 'black';
+                break;
+            case 'white':
+                color = 'white';
+                break;
+        }
+    })
+});
+
+function reset(){
+    const squares = document.querySelectorAll('.square')
+    squares.forEach((square)=>{square.style.backgroundColor = 'white'})
 }
