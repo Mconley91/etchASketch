14e0c968138c4b28a9b4eba503c565ea9b7fdf8c
mconley91 <matthew.e.conley@gmail.com>
2024-10-31 20:45:24 -0400

    add paint toggling feature, tweaked screen resolution

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
 const screen = document.querySelector('#screen');
 const buttons = document.querySelectorAll('.colorButton')
 let color = 'black';
+let areWePainting = false;
 
-for(let i = 0; i < 1000; i++){
+for(let i = 0; i < 20000; i++){
     const square = document.createElement('div');
     square.setAttribute('id', `square${i}`);
     square.setAttribute('class', `square`);
+    square.addEventListener('mouseover', ()=>{painting(square)})
     square.style.backgroundColor = 'white';
-    square.style.width = '2%';
-    square.style.height = '5%';
-    square.addEventListener("mouseover",()=>{square.style.backgroundColor = color})
+    square.style.width = '.5%';
+    square.style.height = '1%';
     screen.appendChild(square);
 };
+
+const squares = document.querySelectorAll('.square')
 
 buttons.forEach((button)=>{
     button.addEventListener('click',(event)=>{
@@ -39,6 +42,12 @@ buttons.forEach((button)=>{
 });
 
 function reset(){
-    const squares = document.querySelectorAll('.square')
     squares.forEach((square)=>{square.style.backgroundColor = 'white'})
 }
+
+function painting(square){
+    if(areWePainting){
+        square.style.backgroundColor = color;
+    }
+}
+screen.addEventListener('click', ()=>{areWePainting ? areWePainting = false : areWePainting = true})
